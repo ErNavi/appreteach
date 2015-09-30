a10f19860322dbfdd023595ee7ab8b1af304383c
defatigue <defatigue@gmail.com>
2015-09-30 07:24:11 +0100

    4

--- login-callback.php
+++ login-callback.php
@@ -27,5 +27,5 @@ if (isset($accessToken)) {
     
     // Now you can redirect to another page and use the
     // access token from $_SESSION['facebook_access_token']
-    header("Location: http://http://appreteach.azurewebsites.net/index.php");
+    header("Location: index.php");
 }
