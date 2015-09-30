a8328c476ad4e7a56aacef0a68eb9f5974b6a04d
defatigue <defatigue@gmail.com>
2015-09-30 07:08:17 +0100

    4

---
+++ login-callback.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+$fb = new Facebook\Facebook([
+    'app_id' => '1480036835633971',
+    'app_secret' => '81daf23dbc2099501a2c1dceb1e7856e',
+    'default_graph_version' => 'v2.2',
+]);
+
+$helper = $fb->getRedirectLoginHelper();
+try {
+    $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+
+if (isset($accessToken)) {
+    // Logged in!
+    $_SESSION['facebook_access_token'] = (string) $accessToken;
+
+    // Now you can redirect to another page and use the
+    // access token from $_SESSION['facebook_access_token']
+}
---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+$fb = new Facebook\Facebook([
+    'app_id' => '1480036835633971',
+    'app_secret' => '81daf23dbc2099501a2c1dceb1e7856e',
+    'default_graph_version' => 'v2.2',
+]);
+
+$helper = $fb->getRedirectLoginHelper();
+$permissions = ['email', 'user_likes']; // optional
+$loginUrl = $helper->getLoginUrl('http://{your-website}/login-callback.php', $permissions);
+
+echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
