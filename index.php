915789222ef99a17529c794b4df056d9e94e532e
defatigue <defatigue@gmail.com>
2015-09-30 06:43:05 +0100

    3

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
-<?php
-?>
+
 <html>
 <head>
+<title>Appreteach</title>
 </head>
 <body>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1480036835633971',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 <div
   class="fb-like"
   data-share="true"
