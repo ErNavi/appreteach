b771aeb665b20ffa2a7d826344621606bb5d548c
defatigue <defatigue@gmail.com>
2015-09-30 08:55:46 +0100

    4

--- index.php
+++ index.php
@@ -33,6 +33,6 @@ echo 'Logged in as ' . $userId;
 
 <html>
 <body>
-<img alt="profile" src="<?php echo "https://graph.facebook.com/".$userId."/picture?type=large" ?>"/>
+<img alt="profile" src="<?php echo "https://graph.facebook.com/".$userId."/picture?type=medium" ?>"/>
 </body>
 </html>
